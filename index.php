f5da8717924a4573f80644028d21d394ac4602fa
Richie-Z <zakariarichie@yahoo.com>
2020-10-15 17:52:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<main id="content">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part( 'entry' ); ?>
+		<?php comments_template(); ?>
+	<?php endwhile; endif; ?>
+	<?php get_template_part( 'nav', 'below' ); ?>
+</main>
+<script type="text/javascript">
+	window.addEventListener('scroll',function(){
+		var header =document.querySelector('header');
+		header.classList.toggle('sticky',window.scrollY>0);
+	})
+</script>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
