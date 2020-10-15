71e4bd0b5a9f1b5cab05a215e6a376cdc150efe6
Richie-Z <zakariarichie@yahoo.com>
2020-10-15 19:31:21 +0700

    style.css updated

--- header.php
+++ header.php
@@ -19,10 +19,33 @@
 			<ul id="menu-nav">
 				<li><a href="#home">Home</a></li>
 				<li><a href="#about">About</a></li>
-				<li><a href="#contact">Contact Us</a></li>
+				<li><a href="#content">Event</a></li>
+				<li><a href="#contact">Sponsor</a></li>
+				<li><a href="#contact">Contact</a></li>
 			</ul>
 			<nav id="menu">
 				<div id="search"><?php get_search_form(); ?></div>
 			</nav>
 		</header>
+		<section id="home">
+		</section>
+		<section id="about">
+			<div class="heading">
+				<h2>About Event-X</h2>
+			</div>
+			<div id="contentBx">
+				<div id="aboutBx">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div id="w50">
+					<img src="<?php echo get_bloginfo('template_url') ?>/img/music-fest.jpeg"/>
+				</div>
+			</div>
+
+		</section>
 		<div id="container">
