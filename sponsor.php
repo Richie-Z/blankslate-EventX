8df8ba6ef0cc7e5979595c1c03e4e314ee1edf7f
Richie-Z <zakariarichie@yahoo.com>
2020-10-18 10:17:48 +0700

    updated

--- sponsor.php
+++ sponsor.php
@@ -3,5 +3,13 @@
 		<div class="heading">
 			<h2>Sponsor</h2>
 		</div>
+		<div id="imgBx">
+			<img src="<?php echo get_bloginfo('template_url') ?>/img/sponsors/magic-fm.png"/>
+			<img src="<?php echo get_bloginfo('template_url') ?>/img/sponsors/monstercat.png"/>
+			<img src="<?php echo get_bloginfo('template_url') ?>/img/sponsors/radio-thailand.png"/>
+			<img src="<?php echo get_bloginfo('template_url') ?>/img/sponsors/vevo.png"/>
+			<img src="<?php echo get_bloginfo('template_url') ?>/img/sponsors/wism-fm.png"/>
+			<img src="<?php echo get_bloginfo('template_url') ?>/img/sponsors/youtube.png"/>
+		</div>
 	</div>
 </section>
